9e353b0d66984f265c8bf8027cfcfae3befdc28d
wrich-crypto <416116073@qq.com>
2023-06-16 23:11:52 +0800

    test

--- test.py
+++ test.py
@@ -13,7 +13,23 @@ import os
 import time
 import uuid
 
-call_back_server_domain = 'https://www.blk123.com/api'
+# 读取配置文件
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+# 设置变量
+MCHID = config['MCHID']
+PRIVATE_KEY_PATH = config['PRIVATE_KEY_PATH']
+CERT_SERIAL_NO = config['CERT_SERIAL_NO']
+APIV3_KEY = config['APIV3_KEY']
+APPID = config['APPID']
+APPID_SECRET = config['APPID_SECRET']
+NOTIFY_URL = config['NOTIFY_URL']
+CERT_DIR = config['CERT_DIR']
+LOGGER_NAME = config['LOGGER']
+PARTNER_MODE = config['PARTNER_MODE']
+PROXY = config['PROXY']
+call_back_server_domain = config['call_back_server_domain']
 
 def request_payment_notify(amount, out_trade_no):
     url = f"{call_back_server_domain}/user/pay/notify"
